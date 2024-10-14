629da02a93d0b5f5cc5e20e5dacbeb7d83f3f66d
ZahidHasan321 <dev.zahidhasan@gmail.com>
2024-10-14 15:33:29 +0600

    lualine changed

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,4 +4,9 @@ return {
   { "rafamadriz/friendly-snippets", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
 }
